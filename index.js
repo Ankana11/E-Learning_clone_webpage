d361be7aa1f2b4a5eb5fe72825a17c4664798a84
Ankana <reach2ankana@gmail.com>
2023-08-03 20:14:47 +0530

    initial commited

---
+++ index.js
@@ -0,0 +1,14 @@
+// burger-------------------------------------------------
+
+burger=document.querySelector('.burger')
+
+navbar=document.querySelector('.navbar')
+navlist=document.querySelector('.navlist')
+
+
+burger.addEventListener('click',()=>{
+    
+     navlist.classList.toggle('v-class-resp')
+     navbar.classList.toggle('h-nav-resp')
+
+})
